4a27515f75b2f23d982b4027db80aea881f3e2eb
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-02-10 21:50:02 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
-function App() {
-  return <h1>Hello, World!</h1>
+import Header from "./components/Header"
+import Main from "./components/Main"
+
+const App = () => {
+  return (
+  <>
+  <Header/>
+  <Main/>;
+  </>
+  )
 }
 
 export default App
